5667188942e8a6040e270178e4141ba310bc74d9
mayuka713 <mayuka.yt63@gmail.com>
2025-02-08 21:52:36 +0900

    fix

--- src/db.ts
+++ src/db.ts
@@ -10,14 +10,15 @@ console.log('DB_PORT:', process.env.DB_PORT);
 console.log('DB_NAME:', process.env.DB_NAME);
 
 
-// PostgreSQLのPool接続
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT) || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
 });
+
 
 
 
