094ec5303c70e0d7815e35b5db517d9823497315
mayuka713 <mayuka.yt63@gmail.com>
2025-02-09 13:16:52 +0900

    fix

--- dist/db.js
+++ dist/db.js
@@ -11,12 +11,12 @@ console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 console.log('DB_HOST:', process.env.DB_HOST);
 console.log('DB_PORT:', process.env.DB_PORT);
 console.log('DB_NAME:', process.env.DB_NAME);
-// PostgreSQLのPool接続
 const pool = new pg_1.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT) || 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.DATABASE_URL
+        ? {
+            rejectUnauthorized: false,
+        }
+        : false,
 });
 exports.default = pool;
